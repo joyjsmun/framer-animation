bf7f29d1ad0b25839c3bcbbd69b7574a7e7ea50a
joyjsmun <joy.js.mun@gmail.com>
2022-02-13 20:50:56 -0800

    ADD: dragging practice

--- src/App.tsx
+++ src/App.tsx
@@ -13,20 +13,23 @@ const Box = styled(motion.div)`
   width:200px;
   height:200px;
   background-color: white;
-  border-radius: 10px;
+  border-radius: 20px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+  
 `
 
-const myVars = {
-  start:{scale:0},
-  end:{scale:1,rotateZ:360, transition:{type:"spring",delay:0.5}},
+
+const boxVariants = {
+ hover:{scale:2,rotateZ:180},
+ click:{borderRadius:"100px",scale:1},
+ drag:{backgroundColor:"rgb(255, 121, 121)", transition:{duration:10}},
 }
 
 
 function App() {
   return (
     <Wrapper>
-    <Box variants={myVars} initial="start" animate="end"/>
+    <Box drag variants={boxVariants} whileDrag="drag" whileHover="hover" whileTap="click"/>
   </Wrapper>
   );
 }
